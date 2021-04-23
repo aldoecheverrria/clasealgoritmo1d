46de6adbfea9d3b7ca039a4dbee11755ffd03f97
aldoecheverria <aldoecheverria@yahoo.com.mx>
2021-04-22 21:42:52 -0500

    trying to flex like whatsapp

--- src/App2.js
+++ src/App2.js
@@ -3,20 +3,113 @@ import './App2.css'
 
 export default function App2() {
     return (
-        <body>
-  <article>
-     <header>
-       <h1>A really good blog article</h1>
-       <p class="subtitle">A short description explaining why this is a good article and trying to get you to stick around </p>
-    </header>
-    <div class="container">
-      <p class="lead" >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
-      <p class="lead"> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-      <p class="lead">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-      <p class="lead">There are many variations of passages of Lorem Ipsum available, but the majority have suffered <strong>alteration in some form </strong>, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the <a>Internet tend </a>to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-    </div>
-  </article>
-  
-</body>
+    <body>
+        <div className="big--container">
+            <div className="cards">
+                <article className="card">
+                <header>
+                    <h2>A short heading</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p> The idea of reaching the North Pole by means of balloons appears to have been entertained many
+                        years ago. The idea of reaching the North Pole by means of balloons appears to have been entertained many
+                        years ago.</p>
+                </div>
+
+            </article>
+
+            <article className="card">
+                <header>
+                    <h2>A short heading</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p>Short content.</p>
+                </div>
+                <footer>I have a footer!</footer>
+            </article>
+
+            <article className="card">
+                <header>
+                    <h2>A longer heading in this card</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p>In a curious work, published in Paris in 1863 by Delaville Dedreux, there is a
+                        suggestion for reaching the North Pole by an aerostat.</p>
+                </div>
+                <footer>I have a footer!</footer>
+            </article>
+            <article className="card">
+                <header>
+                    <h2>A short heading</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p> The idea of reaching the North Pole by means of balloons appears to have been entertained many
+                        years ago. </p>
+                </div>
+
+            </article>
+
+            <article className="card">
+                <header>
+                    <h2>A longer heading in this card</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p>In a curious work, published in Paris in 1863 by Delaville Dedreux, there is a
+                        suggestion for reaching the North Pole by an aerostat.</p>
+                </div>
+                <footer>I have a footer!</footer>
+            </article>
+            <article className="card">
+                <header>
+                    <h2>A short heading</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p> The idea of reaching the North Pole by means of balloons appears to have been entertained many
+                        years ago. </p>
+                </div>
+
+            </article>
+            <article className="card">
+                <header>
+                    <h2>A longer heading in this card</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p>In a curious work, published in Paris in 1863 by Delaville Dedreux, there is a
+                        suggestion for reaching the North Pole by an aerostat.</p>
+                </div>
+                <footer>I have a footer!</footer>
+            </article>
+            <article className="card">
+                <header>
+                    <h2>A short heading</h2>
+                </header>
+
+                <img src="https://mdn.github.io/css-examples/css-cookbook/balloons.jpg" alt="Hot air balloons"/>
+                <div className="content">
+                    <p> The idea of reaching the North Pole by means of balloons appears to have been entertained many
+                        years ago. </p>
+                </div>
+
+            </article>
+
+            </div>
+            <div className="helper"></div>
+        </div>
+
+    </body>
     )
 }
